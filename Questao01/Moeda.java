52ebe40767b94cd4787520b1bcfe60cef772b6d2
Talita Gomes <talita.gomes2027@gmail.com>
2020-04-26 12:40:00 -0400

    implements cambio

--- Questao01/Moeda.java
+++ Questao01/Moeda.java
@@ -4,30 +4,33 @@ A classe moeda deve ter um construtor que inicie os atributos nome e valor
 da moeda e, além dos métodos da interface, os métodos set. 
 */
 
-public class Moeda {
+public class Moeda implements Cambio {
+    private String nome;
+    private Float valor;
 
-    String nome;
-    Float valor;
-
-    public Moeda (String nome, Float valor){
+    public Moeda (String nome, Float valor) {
         this.nome = nome;
         this.valor = valor;
     }
+    
+	@Override
+	public String getNome() {
+		return nome;
+	}
 
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
+	@Override
+	public void setNome(String nome) {
         this.nome = nome;
-    }
+		
+	}
 
-    public Float getValor() {
-        return this.valor;
-    }
+	@Override
+	public Float getValor() {
+		return valor;
+	}
 
-    public void setValor(Float valor) {
-        this.valor = valor;
-    }
-
+	@Override
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
 }
